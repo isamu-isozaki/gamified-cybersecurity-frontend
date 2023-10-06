f04490e1d94e359eedd68b07b05cf7747cc4864e
isamu-isozaki <isamu.website@gmail.com>
2023-10-06 16:54:30 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/**
+ * Author: Isamu Isozaki
+ */
+import React from 'react';
+import Home from './app/screens/Home';
+
+/**
+ * Initialize firebase and go to AuthNavigator
+ */
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
