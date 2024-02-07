3b811b1d64bc08b0580e6e4c6bc0b3aba5b9c08b
chi-p-nguyen <phuongchi2782001@gmail.com>
2024-02-07 10:54:32 -0500

    Combine socket code with others

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
  * Author: Isamu Isozaki
  */
 import React from 'react';
-import TerminalGCS from "./app/components/TerminalGCS";
 import Console from './app/screens/Console';
 import Home from './screens/Home';
 import io from 'socket.io-client';
+
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:10000';
 
 export const socket = io(URL);
