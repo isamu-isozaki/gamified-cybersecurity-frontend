1410ec7ebce443bb03e6a49680db4c8b8eb33421
Ryan Dixon <rjd323@drexel.edu>
2024-02-21 14:59:25 -0500

    environment variables for better backend sync

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: true,
+  },
+  preview: {
+    port: 3000,
+    host: true,
   },
   resolve: {
     alias: {
